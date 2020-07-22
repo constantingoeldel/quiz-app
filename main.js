2f80dc47b5e8a37c821d8cfd74e6d1d6665bdde0
constantingoeldel <constantin_goeldel@t-online.de>
2020-07-22 16:07:26 +0200

    site switching with js working

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 "use strict";
 
-console.log("It works!");
-console.log("Even with Babel:Watch");
-console.log("And aslo with npm start");
-console.log("And in the browser");
+// Select sites
+var index = get(".index");
+var bookmarks = get(".bookmarks");
+var create = get(".create");
+var profile = get(".profile"); // Select Nav-Items
+
+var navIndex = get(".nav__index");
+var navBookmarks = get(".nav__bookmarks");
+var navCreate = get(".nav__create");
+var navProfile = get(".nav__profile"); // Nav Event Listeners
+
+navIndex.addEventListener("click", function () {
+  nav(index);
+});
+navBookmarks.addEventListener("click", function () {
+  nav(bookmarks);
+});
+navCreate.addEventListener("click", function () {
+  nav(create);
+});
+navProfile.addEventListener("click", function () {
+  nav(profile);
+}); // Show/Hide pages
+
+function nav(newSite) {
+  index.classList.add("hidden");
+  bookmarks.classList.add("hidden");
+  create.classList.add("hidden");
+  profile.classList.add("hidden");
+  newSite.classList.remove("hidden");
+} // Select Items shortend
+
+
+function get(selector) {
+  return document.querySelector(selector);
+}
---
+++ src/main.js
@@ -0,0 +1,38 @@
+// Select sites
+const index = get(".index");
+const bookmarks = get(".bookmarks");
+const create = get(".create");
+const profile = get(".profile");
+
+// Select Nav-Items
+const navIndex = get(".nav__index");
+const navBookmarks = get(".nav__bookmarks");
+const navCreate = get(".nav__create");
+const navProfile = get(".nav__profile");
+
+// Nav Event Listeners
+navIndex.addEventListener("click", () => {
+  nav(index);
+});
+navBookmarks.addEventListener("click", () => {
+  nav(bookmarks);
+});
+navCreate.addEventListener("click", () => {
+  nav(create);
+});
+navProfile.addEventListener("click", () => {
+  nav(profile);
+});
+
+// Show/Hide pages
+function nav(newSite) {
+  index.classList.add("hidden");
+  bookmarks.classList.add("hidden");
+  create.classList.add("hidden");
+  profile.classList.add("hidden");
+  newSite.classList.remove("hidden");
+}
+// Select Items shortend
+function get(selector) {
+  return document.querySelector(selector);
+}
