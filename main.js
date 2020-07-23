2303d880d1ceb884c164b084d2534e375bcd4248
constantingoeldel <constantin_goeldel@t-online.de>
2020-07-23 09:28:46 +0200

    bookmark, darkmode

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
 "use strict";
 
 // Select sites
-var index = get(".index");
-var bookmarks = get(".bookmarks");
-var create = get(".create");
-var profile = get(".profile"); // Select Nav-Items
+var index = get('.index');
+var bookmarks = get('.bookmarks');
+var create = get('.create');
+var profile = get('.profile'); // Select Nav-Items
 
-var navIndex = get(".nav__index");
-var navBookmarks = get(".nav__bookmarks");
-var navCreate = get(".nav__create");
-var navProfile = get(".nav__profile"); // Answer Toggle
+var navIndex = get('.nav__index');
+var navBookmarks = get('.nav__bookmarks');
+var navCreate = get('.nav__create');
+var navProfile = get('.nav__profile'); // Answer Toggle
 
-var answerButton = get(".card__button");
-var answerText = get(".card__answer");
-answerButton.addEventListener("click", function () {
-  answerText.classList.toggle("blurred");
+var answerButton = get('.card__button');
+var answerText = get('.card__answer');
+answerButton.addEventListener('click', function () {
+  answerText.classList.toggle('blurred');
+}); //bookmark toggle
+
+var bookmark = get('.card__bookmark');
+bookmark.addEventListener('click', function () {
+  bookmark.classList.toggle('card__bookmark--active');
+}); // Darkmode
+
+var body = get('body');
+var header = get('header');
+var svg = get('svg');
+var darkmodeSwitch = get('.button__darkmode');
+darkmodeSwitch.addEventListener('click', function () {
+  body.classList.toggle('darkmode');
+  header.classList.toggle('darkmode');
+  svg.classList.toggle('svg__darkmode');
 }); // Nav Event Listeners
 
-navIndex.addEventListener("click", function () {
+navIndex.addEventListener('click', function () {
   nav(index);
 });
-navBookmarks.addEventListener("click", function () {
+navBookmarks.addEventListener('click', function () {
   nav(bookmarks);
 });
-navCreate.addEventListener("click", function () {
+navCreate.addEventListener('click', function () {
   nav(create);
 });
-navProfile.addEventListener("click", function () {
+navProfile.addEventListener('click', function () {
   nav(profile);
 }); // Show/Hide pages
 
 function nav(newSite) {
-  index.classList.add("hidden");
-  bookmarks.classList.add("hidden");
-  create.classList.add("hidden");
-  profile.classList.add("hidden");
-  newSite.classList.remove("hidden");
+  index.classList.add('hidden');
+  bookmarks.classList.add('hidden');
+  create.classList.add('hidden');
+  profile.classList.add('hidden');
+  newSite.classList.remove('hidden');
 } // Select Items shortend
 
 
--- src/main.js
+++ src/main.js
@@ -1,45 +1,60 @@
 // Select sites
-const index = get(".index");
-const bookmarks = get(".bookmarks");
-const create = get(".create");
-const profile = get(".profile");
+const index = get('.index')
+const bookmarks = get('.bookmarks')
+const create = get('.create')
+const profile = get('.profile')
 
 // Select Nav-Items
-const navIndex = get(".nav__index");
-const navBookmarks = get(".nav__bookmarks");
-const navCreate = get(".nav__create");
-const navProfile = get(".nav__profile");
+const navIndex = get('.nav__index')
+const navBookmarks = get('.nav__bookmarks')
+const navCreate = get('.nav__create')
+const navProfile = get('.nav__profile')
 
 // Answer Toggle
-const answerButton = get(".card__button");
-const answerText = get(".card__answer");
-answerButton.addEventListener("click", () => {
-  answerText.classList.toggle("blurred");
-});
+const answerButton = get('.card__button')
+const answerText = get('.card__answer')
+answerButton.addEventListener('click', () => {
+  answerText.classList.toggle('blurred')
+})
+//bookmark toggle
+const bookmark = get('.card__bookmark')
+bookmark.addEventListener('click', () => {
+  bookmark.classList.toggle('card__bookmark--active')
+})
 
+// Darkmode
+const body = get('body')
+const header = get('header')
+const svg = get('svg')
+const darkmodeSwitch = get('.button__darkmode')
+darkmodeSwitch.addEventListener('click', () => {
+  body.classList.toggle('darkmode')
+  header.classList.toggle('darkmode')
+  svg.classList.toggle('svg__darkmode')
+})
 // Nav Event Listeners
-navIndex.addEventListener("click", () => {
-  nav(index);
-});
-navBookmarks.addEventListener("click", () => {
-  nav(bookmarks);
-});
-navCreate.addEventListener("click", () => {
-  nav(create);
-});
-navProfile.addEventListener("click", () => {
-  nav(profile);
-});
+navIndex.addEventListener('click', () => {
+  nav(index)
+})
+navBookmarks.addEventListener('click', () => {
+  nav(bookmarks)
+})
+navCreate.addEventListener('click', () => {
+  nav(create)
+})
+navProfile.addEventListener('click', () => {
+  nav(profile)
+})
 
 // Show/Hide pages
 function nav(newSite) {
-  index.classList.add("hidden");
-  bookmarks.classList.add("hidden");
-  create.classList.add("hidden");
-  profile.classList.add("hidden");
-  newSite.classList.remove("hidden");
+  index.classList.add('hidden')
+  bookmarks.classList.add('hidden')
+  create.classList.add('hidden')
+  profile.classList.add('hidden')
+  newSite.classList.remove('hidden')
 }
 // Select Items shortend
 function get(selector) {
-  return document.querySelector(selector);
+  return document.querySelector(selector)
 }
